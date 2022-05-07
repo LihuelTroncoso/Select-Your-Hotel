7f7315702e377b249793cf872917bdc4c9dd2f3b
Fabian Lihuel Troncoso <lihueltroncoso@gmail.com>
2022-05-07 13:41:18 -0300

    navigation bar

--- web/application/views/pages/home.php
+++ web/application/views/pages/home.php
@@ -1,8 +1,2 @@
 <h3>Bienvenido a Mudanzas Relokito, las mudanzas mas rápidas y confiables de Argentina</h3>
 <p>Puedes hacer encargos llamando a nuestro numero: 11123456789</p>
-
-<div class="d-flex flex-column">
-    <div>aaa</div>
-    <div>aaaaaa</div>
-    <div>aaaaaaaaaaaaaa</div>
-</div>
